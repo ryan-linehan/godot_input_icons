471c9a0faf153fe260d7e4fae5c62d6386f9def1
RcubDev <rlinehan2@gmail.com>
2025-04-29 21:37:22 -0700

    add more controller button exports to controller icons resource

--- godot_input_icons/addons/godot_input_icons/resources/controller_icons.gd
+++ godot_input_icons/addons/godot_input_icons/resources/controller_icons.gd
@@ -32,3 +32,21 @@ extends Resource
 @export_category("Analog Sticks")
 @export var left_stick_in: Texture2D
 @export var right_stick_in: Texture2D
+
+@export_category("Face Button Extras")
+@export var face_button_up_extra: Texture2D
+@export var face_button_right_extra: Texture2D
+@export var face_button_down_extra: Texture2D
+@export var face_button_left_extra: Texture2D
+
+@export_category("Others")
+## Corresponds to the Sony PS, Xbox Home button.
+@export var guide_button: Texture2D
+## Corresponds to Xbox share button, PS5 microphone button, Nintendo Switch capture button.
+@export var misc1: Texture2D
+## Game controller SDL touchpad button (the big button on PS5)
+@export var touchpad: Texture2D
+@export var paddle1: Texture2D
+@export var paddle2: Texture2D
+@export var paddle3: Texture2D
+@export var paddle4: Texture2D
