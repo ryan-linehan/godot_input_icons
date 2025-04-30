a664cbad4741a7d991ec5bbcbe5b9fb8e5a051ab
RcubDev <rlinehan2@gmail.com>
2025-04-29 21:36:51 -0700

    added unmapped key + controller button values to InputIconMap resource

--- godot_input_icons/addons/godot_input_icons/resources/input_icon_map.gd
+++ godot_input_icons/addons/godot_input_icons/resources/input_icon_map.gd
@@ -6,3 +6,9 @@ const INPUT_TYPES = preload("res://addons/godot_input_icons/input_icon_constants
 
 @export var keyboard_icons: KeyboardIcons
 @export var controller_icons: Dictionary[INPUT_TYPES, ControllerIcons]
+## If provided this texture is displayed when the keyboard key is unmapped
+## NOTE: Will also display if the button is unsupported by the plugin
+@export var unmapped_key: Texture2D
+## If provided this texture is displayed when the joypad button is unmapped
+## NOTE: Will also display if the button is unsupported by the plugin
+@export var unmapped_controller_button: Texture2D
